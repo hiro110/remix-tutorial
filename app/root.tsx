cb436ad77f17df788d8f5dbfd8f8ec495ae454ca
hito-flr <hiroaki.ito@flierinc.com>
2024-05-09 00:14:14 +0000

    fixed: 日本語ベース

--- app/root.tsx
+++ app/root.tsx
@@ -16,11 +16,12 @@ export const links: LinksFunction = () => [
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="ja">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
+        {/* <link rel="stylesheet" href="app.css" /> */}
         <Links />
       </head>
       <body>
