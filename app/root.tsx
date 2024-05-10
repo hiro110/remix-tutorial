5f23910944e5f4304bae993fcde3a5d43eff1c9b
hito-flr <hiroaki.ito@flierinc.com>
2024-05-10 09:38:43 +0000

    add: ./data.tsからサイドバー情報を取得するようにした（loader追加）

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,16 @@
 import type { LinksFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
 import {
   Form,
+  Link,
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
+import { getContacts } from "./data";
 
 import appStylesHref from "./app.css?url";
 
@@ -14,7 +18,14 @@ export const links: LinksFunction = () => [
   { rel: "stylesheet", href: appStylesHref },
 ];
 
+export const loader = async () => {
+  const contacts = await getContacts();
+  return json({ contacts });
+};
+
 export default function App() {
+  const { contacts } = useLoaderData<typeof loader>();
+  
   return (
     <html lang="ja">
       <head>
@@ -43,14 +54,38 @@ export default function App() {
             </Form>
           </div>
           <nav>
-            <ul>
+            {/* <ul>
               <li>
-                <a href={`/contacts/1`}>Your Name</a>
+                <Link to={`/contacts/1`}>Your Name</Link>
               </li>
               <li>
-                <a href={`/contacts/2`}>Your Friend</a>
+                <Link to={`/contacts/2`}>Your Friend</Link>
               </li>
-            </ul>
+            </ul> */}
+            {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <Link to={`contacts/${contact.id}`}>
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}{" "}
+                      {contact.favorite ? (
+                        <span>★</span>
+                      ) : null}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
           </nav>
         </div>
         <div id="detail">
